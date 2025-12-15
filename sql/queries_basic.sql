70867662eeeb4afb8cbdf5afc7e402ca0c159f13
merrinjohn123 <merrinjohn94@gmail.com>
2025-12-15 12:47:42 +0530

    Sales by product

--- sql/queries_basic.sql
+++ sql/queries_basic.sql
@@ -4,3 +4,10 @@ select count(distinct o.order_id) as Tot_orders,sum(line_amount) as Tot_revenue 
 join orders o
 on ot.order_id=o.order_id
 where o.order_status not in ('Cancelled');
+
+--Total quantity sold and total revenue per product.
+select p.name,sum(ot.quantity) as tot_quantity,sum(ot.line_amount) as tot_revenue
+from ORDER_ITEMS ot
+join products p
+on p.product_id=ot.product_id
+group by p.name;
