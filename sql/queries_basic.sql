6cff93fef0a78c20fad12e5410f716314eb7f695
merrinjohn123 <merrinjohn94@gmail.com>
2025-12-17 18:47:01 +0530

    Sales per month

--- sql/queries_basic.sql
+++ sql/queries_basic.sql
@@ -19,3 +19,11 @@ join products p
 on p.product_id=ot.product_id
 group by p.category
 order by tot_revenue desc ;
+
+--Total sales per month for non-cancelled orders
+Select to_char(o.order_date,'MM-YY') as month,sum(ot.line_amount) as revenue
+from order_items ot
+join orders o
+on ot.order_id=o.ORDER_ID
+where o.order_status not in ('Cancelled')
+group by to_char(o.order_date,'MM-YY');
